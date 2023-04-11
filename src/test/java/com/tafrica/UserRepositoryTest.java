c591ec7cd54ab019dbcf082f5dcda5e9147e592c
Pedzi Tokwe <tokwepedzi@gmail.com>
2023-04-11 22:48:39 +0100

    implemented additional user repository tests

--- src/test/java/com/tafrica/UserRepositoryTest.java
+++ src/test/java/com/tafrica/UserRepositoryTest.java
@@ -2,12 +2,13 @@ package com.tafrica;
 
 import com.tafrica.user.User;
 import com.tafrica.user.UserRepository;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,10 +17,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 public class UserRepositoryTest {
-    @Autowired private UserRepository repository;
+    @Autowired
+    private UserRepository repository;
 
     @Test
-    public void testAddNew(){
+    public void testAddNew() {
         User user = new User();
         user.setEmail("tokwepedzi@gmail.com");
         user.setPassword("140989pedzi");
@@ -29,6 +31,23 @@ public class UserRepositoryTest {
         User savedUser = repository.save(user);
 
         //assertions
-        //Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll() {
+        Iterable<User> users = repository.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    //Test for updating the user
+    @Test
+    public void testUpdate() {
+
     }
 }
